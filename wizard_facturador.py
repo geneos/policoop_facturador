cd2824320ebdd7121d00eac337bce364cf266c24
madilisio <madilisio@yahoo.com.ar>
2018-06-05 15:23:18 -0300

    iva

--- wizard_facturador.py
+++ wizard_facturador.py
@@ -260,7 +260,7 @@ class CreadorFacturas(object):
                 for i in sale.lines:                 
                             
 
-                    tax_browse_records = Tax.search([('name','=', 'IVA 21% Ventas   ')])
+                    tax_browse_records = Tax.search([('name','=', 'IVA 21% Ventas')])
                     #tax_browse_records = Tax.browse([2]) or []                                            
                     i.taxes = tuple(tax_browse_records)
                     i.save()
